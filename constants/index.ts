cf8f83b598c8b819654fca6cd8ca597e093bce37
MounikaMonika <seepanamounika2002@gmail.com>
2024-07-28 14:49:35 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,42 @@
+export const sidebarLinks=[
+    {
+        label:'Home',
+        imgURL:'/icons/Home.svg',
+        route:'/',
+       
+    },
+    {
+        label: 'Upcoming',
+        imgURL: '/icons/upcoming.svg',
+        route: '/upcoming',
+  
+      },
+      {
+        label: 'Previous',
+        imgURL: '/icons/previous.svg',
+        route: '/previous',
+        
+      },
+      {
+        label: 'Recordings',
+        imgURL: '/icons/Video.svg',
+        route: '/recordings',
+        
+      },
+      {
+        label: 'Personal Room',
+        imgURL: '/icons/add-personal.svg',
+        route: '/personal-room',
+        
+      },
+]
+
+export const avatarImages=[
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
+]
+
+
